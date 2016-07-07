e480f9276ee79993b09cb2f72a130a26f55f3279
GeorgeTamate <georgetamate@gmail.com>
2016-07-07 08:40:35 -0400

    Se agrego codigo para viajar a diferentes ciudades.

--- js/client_world.js
+++ js/client_world.js
@@ -1,24 +1,52 @@
+//--------------------------------------------------------------//
+//---------------------- GLOBAL VARIABLES ----------------------//
+//--------------------------------------------------------------//
+
+// Display Elements------------------
 var container, raycaster, objects = [];
 var renderer, scene, camera, controls, effect;
+var zoom = 0;
+
+// VR-Control Camera Position------------------
+var cx = 0;
+var cy = 3;
+var cz = 0;
+
+// Reticle------------------
 var reticle;
+var timebuf = 0;
+
+// Image Loader-----------------
 var loader;
+
+// Keyboard Elements-----------------
 var keyState = {};
+
+// Scene Objects------------------
+var plane;
+var sky, sunSphere;
 var sphere;
 
-var plane;
+// City Cubes------------------
 var cube;
 var cubes = [];
-var sky, sunSphere;
+var londonCube, parisCube, tokyoCube, turkeyCube, nycCube, sdCube;
+  // city cube materials
+var londonFaceMaterial = [];
+var parisFaceMaterial = [];
+var tokyoFaceMaterial = [];
+var turkeyFaceMaterial = [];
+var nycFaceMaterial = [];
+var sdFaceMaterial = [];
 
+// Players------------------
 var player, playerId, moveSpeed, turnSpeed;
 var playerData;
 var otherPlayers = [], otherPlayersId = [];
-var zoom = 0;
 
-var timebuf = 0;
+
 
 var loadWorld = function(){
-
   //----------------------------------------------------------//
   //---------------------- INITIALIZING ----------------------//
   //----------------------------------------------------------//
@@ -49,137 +77,186 @@ var loadWorld = function(){
   initSky();
 
   //Plane------------------
-  var planeGeometry = new THREE.PlaneGeometry(40,40);
+  var planeGeometry = new THREE.PlaneGeometry(100,100);
   var planeMaterial = new THREE.MeshBasicMaterial({color: 0x904C3E, side: THREE.DoubleSide});
   plane = new THREE.Mesh(planeGeometry,planeMaterial);
   // rotate and position the plane
   plane.rotation.x=-0.5*Math.PI;
-  plane.position.x=15
+  plane.position.x=0
   plane.position.y=-0.5
   plane.position.z=0
   // add the plane to the scene
   scene.add(plane);
 
   //Cubes------------------
-  // Create 3D objects.
   var geometry = new THREE.CubeGeometry(1, 1, 1, 1, 1, 1);
-  //var material = new THREE.MeshNormalMaterial();
-
-  // texture arrays
-  var materialsA = [];
-  var materialsB = [];
-  var materialsC = [];
-  var materialsD = [];
-  var materialsE = [];
-  var materialsF = [];
 
   // loading texture imgs and pushing them to arrays
-  materialsA.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ln/ln1.jpg')} )); //LONDON
-  materialsA.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ln/ln2.jpg')} ));
-  materialsA.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ln/ln3.png')} ));
-  materialsA.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ln/ln4.jpg')} ));
-  materialsA.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ln/ln5.jpg')} ));
-  materialsA.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ln/ln6.jpg')} ));
+  londonFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ln/ln1.jpg')} )); //LONDON
+  londonFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ln/ln2.jpg')} ));
+  londonFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ln/ln3.png')} ));
+  londonFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ln/ln4.jpg')} ));
+  londonFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ln/ln5.jpg')} ));
+  londonFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ln/ln6.jpg')} ));
 
-  materialsB.push(new THREE.MeshBasicMaterial( { map: loader.load('img/fr/fr2.png')} )); //PARIS
-  materialsB.push(new THREE.MeshBasicMaterial( { map: loader.load('img/fr/fr5.jpg')} ));
-  materialsB.push(new THREE.MeshBasicMaterial( { map: loader.load('img/fr/fr1.png')} ));
-  materialsB.push(new THREE.MeshBasicMaterial( { map: loader.load('img/fr/fr1.png')} ));
-  materialsB.push(new THREE.MeshBasicMaterial( { map: loader.load('img/fr/fr4.jpg')} ));
-  materialsB.push(new THREE.MeshBasicMaterial( { map: loader.load('img/fr/fr6.png')} ));
+  parisFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/fr/fr2.png')} )); //PARIS
+  parisFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/fr/fr5.jpg')} ));
+  parisFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/fr/fr1.png')} ));
+  parisFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/fr/fr1.png')} ));
+  parisFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/fr/fr4.jpg')} ));
+  parisFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/fr/fr6.png')} ));
 
-  materialsC.push(new THREE.MeshBasicMaterial( { map: loader.load('img/jp/jp1.jpg')} )); //TOKYO
-  materialsC.push(new THREE.MeshBasicMaterial( { map: loader.load('img/jp/jp2.jpg')} ));
-  materialsC.push(new THREE.MeshBasicMaterial( { map: loader.load('img/jp/jp3.png')} ));
-  materialsC.push(new THREE.MeshBasicMaterial( { map: loader.load('img/jp/jp4.jpg')} ));
-  materialsC.push(new THREE.MeshBasicMaterial( { map: loader.load('img/jp/jp5.jpg')} ));
-  materialsC.push(new THREE.MeshBasicMaterial( { map: loader.load('img/jp/jp6.png')} ));
+  tokyoFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/jp/jp1.jpg')} )); //TOKYO
+  tokyoFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/jp/jp2.jpg')} ));
+  tokyoFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/jp/jp3.png')} ));
+  tokyoFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/jp/jp4.jpg')} ));
+  tokyoFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/jp/jp5.jpg')} ));
+  tokyoFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/jp/jp6.png')} ));
 
-  materialsD.push(new THREE.MeshBasicMaterial( { map: loader.load('img/tk/tk1.jpg')} )); //TURKEY
-  materialsD.push(new THREE.MeshBasicMaterial( { map: loader.load('img/tk/tk2.jpg')} ));
-  materialsD.push(new THREE.MeshBasicMaterial( { map: loader.load('img/tk/tk3.png')} ));
-  materialsD.push(new THREE.MeshBasicMaterial( { map: loader.load('img/tk/tk4.jpg')} ));
-  materialsD.push(new THREE.MeshBasicMaterial( { map: loader.load('img/tk/tk5.jpg')} ));
-  materialsD.push(new THREE.MeshBasicMaterial( { map: loader.load('img/tk/tk6.jpg')} ));
+  turkeyFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/tk/tk1.jpg')} )); //TURKEY
+  turkeyFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/tk/tk2.jpg')} ));
+  turkeyFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/tk/tk3.png')} ));
+  turkeyFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/tk/tk4.jpg')} ));
+  turkeyFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/tk/tk5.jpg')} ));
+  turkeyFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/tk/tk6.jpg')} ));
 
-  materialsE.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ny/ny1.jpg')} )); //NYC
-  materialsE.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ny/ny2.jpg')} ));
-  materialsE.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ny/ny3.png')} ));
-  materialsE.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ny/ny4.jpg')} ));
-  materialsE.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ny/ny5.jpg')} ));
-  materialsE.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ny/ny6.jpg')} ));
+  nycFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ny/ny1.jpg')} )); //NYC
+  nycFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ny/ny2.jpg')} ));
+  nycFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ny/ny3.png')} ));
+  nycFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ny/ny4.jpg')} ));
+  nycFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ny/ny5.jpg')} ));
+  nycFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/ny/ny6.jpg')} ));
 
-  materialsF.push(new THREE.MeshBasicMaterial( { map: loader.load('img/sd/sd4.png')} )); //SD
-  materialsF.push(new THREE.MeshBasicMaterial( { map: loader.load('img/sd/sd7.png')} ));
-  materialsF.push(new THREE.MeshBasicMaterial( { map: loader.load('img/sd/sd2.png')} ));
-  materialsF.push(new THREE.MeshBasicMaterial( { map: loader.load('img/sd/sd1.png')} ));
-  materialsF.push(new THREE.MeshBasicMaterial( { map: loader.load('img/sd/sd6.png')} ));
-  materialsF.push(new THREE.MeshBasicMaterial( { map: loader.load('img/sd/sd5.png')} ));
+  sdFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/sd/sd4.png')} )); //SD
+  sdFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/sd/sd7.png')} ));
+  sdFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/sd/sd2.png')} ));
+  sdFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/sd/sd1.png')} ));
+  sdFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/sd/sd6.png')} ));
+  sdFaceMaterial.push(new THREE.MeshBasicMaterial( { map: loader.load('img/sd/sd5.png')} ));
 
   // applying textures to cubes
-  cubes[0] = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( materialsA ));
-  cubes[1] = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( materialsB ));
-  cubes[2] = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( materialsC ));
-  cubes[3] = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( materialsD ));
-  cubes[4] = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( materialsE ));
-  cubes[5] = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( materialsF ));
+  cubes[0] = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( londonFaceMaterial ));
+  cubes[1] = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( parisFaceMaterial ));
+  cubes[2] = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( tokyoFaceMaterial ));
+  cubes[3] = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( turkeyFaceMaterial ));
+  cubes[4] = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( nycFaceMaterial ));
+  cubes[5] = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( sdFaceMaterial ));
 
+  londonCube = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( londonFaceMaterial ));
+  parisCube = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( parisFaceMaterial));
+  tokyoCube = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( tokyoFaceMaterial ));
+  turkeyCube = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( turkeyFaceMaterial ));
+  nycCube = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( nycFaceMaterial ));
+  sdCube = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial( sdFaceMaterial ));
 
-  /*
-  // reticle event handler functions for cubes
-  cubes[i].ongazelong = function(){
-    //this.material = reticle.get_random_hex_material();
-      if(timebuf < 3) {
-          timebuf++;
-      } else {
-          socket.emit('lookingAtCube', cityCubes[this]);
-          timebuf = 0;
-      }
-  }
-  cubes[i].ongazeover = function(){
-    //this.material = reticle.get_random_hex_material();
-  }
-  cubes[i].ongazeout = function(){
-    //this.material = reticle.default_material();
-    timebuf = 0;
-  }
-  */
 
   cubes[0].ongazelong = function(){
-      if(timebuf < 3) {timebuf++;}
-      else {socket.emit('lookingAtCube', 'London'); timebuf = 0;}}
+      if(timebuf < 2) {timebuf++;}
+      else {
+        socket.emit('lookingAtCube', 'London'); timebuf = 0;
+        flyTo('London');
+      }}
   cubes[0].ongazeover = function(){}
   cubes[0].ongazeout = function(){timebuf = 0;}
 
   cubes[1].ongazelong = function(){
-      if(timebuf < 3) {timebuf++;}
-      else {socket.emit('lookingAtCube', 'Paris'); timebuf = 0;}}
+      if(timebuf < 2) {timebuf++;}
+      else {
+        socket.emit('lookingAtCube', 'Paris'); timebuf = 0;
+        flyTo('Paris');
+      }}
   cubes[1].ongazeover = function(){}
   cubes[1].ongazeout = function(){timebuf = 0;}
 
   cubes[2].ongazelong = function(){
-      if(timebuf < 3) {timebuf++;}
-      else {socket.emit('lookingAtCube', 'Tokyo'); timebuf = 0;}}
+      if(timebuf < 2) {timebuf++;}
+      else {
+        socket.emit('lookingAtCube', 'Tokyo'); timebuf = 0;
+        flyTo('Tokyo');
+      }}
   cubes[2].ongazeover = function(){}
   cubes[2].ongazeout = function(){timebuf = 0;}
 
   cubes[3].ongazelong = function(){
-      if(timebuf < 3) {timebuf++;}
-      else {socket.emit('lookingAtCube', 'Turkey'); timebuf = 0;}}
+      if(timebuf < 2) {timebuf++;}
+      else {
+        socket.emit('lookingAtCube', 'Turkey'); timebuf = 0;
+        flyTo('Turkey');
+      }}
   cubes[3].ongazeover = function(){}
   cubes[3].ongazeout = function(){timebuf = 0;}
 
   cubes[4].ongazelong = function(){
-      if(timebuf < 3) {timebuf++;}
-      else {socket.emit('lookingAtCube', 'NYC'); timebuf = 0;}}
+      if(timebuf < 2) {timebuf++;}
+      else {
+        socket.emit('lookingAtCube', 'NYC'); timebuf = 0;
+        flyTo('NYC');
+      }}
   cubes[4].ongazeover = function(){}
   cubes[4].ongazeout = function(){timebuf = 0;}
 
   cubes[5].ongazelong = function(){
-      if(timebuf < 3) {timebuf++;}
-      else {socket.emit('lookingAtCube', 'Santo Domingo'); timebuf = 0;}}
+      if(timebuf < 2) {timebuf++;}
+      else {
+        socket.emit('lookingAtCube', 'Santo Domingo'); timebuf = 0;
+        flyTo('SD');
+      }}
   cubes[5].ongazeover = function(){}
   cubes[5].ongazeout = function(){timebuf = 0;}
+
+  londonCube.ongazelong = function(){
+      if(timebuf < 2) {timebuf++;}
+      else {
+        socket.emit('lookingAtCube', 'London'); timebuf = 0;
+        flyTo('Home');
+      }}
+  londonCube.ongazeover = function(){}
+  londonCube.ongazeout = function(){timebuf = 0;}
+
+  parisCube.ongazelong = function(){
+      if(timebuf < 2) {timebuf++;}
+      else {
+        socket.emit('lookingAtCube', 'Paris'); timebuf = 0;
+        flyTo('Home');
+      }}
+  parisCube.ongazeover = function(){}
+  parisCube.ongazeout = function(){timebuf = 0;}
+
+  tokyoCube.ongazelong = function(){
+      if(timebuf < 2) {timebuf++;}
+      else {
+        socket.emit('lookingAtCube', 'Tokyo'); timebuf = 0;
+        flyTo('Home');
+      }}
+  tokyoCube.ongazeover = function(){}
+  tokyoCube.ongazeout = function(){timebuf = 0;}
+
+  turkeyCube.ongazelong = function(){
+      if(timebuf < 2) {timebuf++;}
+      else {
+        socket.emit('lookingAtCube', 'Turkey'); timebuf = 0;
+        flyTo('Home');
+      }}
+  turkeyCube.ongazeover = function(){}
+  turkeyCube.ongazeout = function(){timebuf = 0;}
+
+  nycCube.ongazelong = function(){
+      if(timebuf < 2) {timebuf++;}
+      else {
+        socket.emit('lookingAtCube', 'NYC'); timebuf = 0;
+        flyTo('Home');
+      }}
+  nycCube.ongazeover = function(){}
+  nycCube.ongazeout = function(){timebuf = 0;}
+
+  sdCube.ongazelong = function(){
+      if(timebuf < 2) {timebuf++;}
+      else {
+        socket.emit('lookingAtCube', 'Santo Domingo'); timebuf = 0;
+        flyTo('Home');
+      }}
+  sdCube.ongazeover = function(){}
+  sdCube.ongazeout = function(){timebuf = 0;}
 
   // positioning and adding cubes to the scene
   for (var i = 0; i < 6; i++) {
@@ -194,16 +271,100 @@ var loadWorld = function(){
       scene.add(cubes[i]);
   }
 
+  reticle.add_collider(londonCube);
+  londonCube.position.x = -50;
+  londonCube.position.y = 0.5;
+  londonCube.position.z = -50;
+  scene.add(londonCube);
+
+  reticle.add_collider(parisCube);
+  parisCube.position.x = 0;
+  parisCube.position.y = 0.5;
+  parisCube.position.z = -50;
+  scene.add(parisCube);
+
+  reticle.add_collider(tokyoCube);
+  tokyoCube.position.x = 50;
+  tokyoCube.position.y = 0.5;
+  tokyoCube.position.z = -50;
+  scene.add(tokyoCube);
+
+  reticle.add_collider(turkeyCube);
+  turkeyCube.position.x = -50;
+  turkeyCube.position.y = 0.5;
+  turkeyCube.position.z = 50;
+  scene.add(turkeyCube);
+
+  reticle.add_collider(nycCube);
+  nycCube.position.x = 0;
+  nycCube.position.y = 0.5;
+  nycCube.position.z = 50;
+  scene.add(nycCube);
+
+  reticle.add_collider(sdCube);
+  sdCube.position.x = 50;
+  sdCube.position.y = 0.5;
+  sdCube.position.z = 50;
+  scene.add(sdCube);
+
+
+
+  //Events------------------------------------------
+  document.addEventListener('keydown', onKeyDown, false );
+  document.addEventListener('keyup', onKeyUp, false );
 
 
 
 
 
-
-
-
-
-
+  function flyTo(_city) {
+    switch(_city) {
+      case 'Home':
+        player.position.x = 0;
+        player.position.z = 0;
+        updatePlayerData();
+        socket.emit('updatePosition', playerData);
+        break;
+      case 'London':
+        player.position.x = -47;
+        player.position.z = -47;
+        updatePlayerData();
+        socket.emit('updatePosition', playerData);
+        break;
+      case 'Paris':
+        player.position.x = 0;
+        player.position.z = -47;
+        updatePlayerData();
+        socket.emit('updatePosition', playerData);
+        break;
+      case 'Tokyo':
+        player.position.x = 47;
+        player.position.z = -47;
+        updatePlayerData();
+        socket.emit('updatePosition', playerData);
+        break;
+      case 'Turkey':
+        player.position.x = -47;
+        player.position.z = 47;
+        updatePlayerData();
+        socket.emit('updatePosition', playerData);
+        break;
+      case 'NYC':
+        player.position.x = 0;
+        player.position.z = 47;
+        updatePlayerData();
+        socket.emit('updatePosition', playerData);
+        break;
+      case 'SD':
+        player.position.x = 47;
+        player.position.z = 47;
+        updatePlayerData();
+        socket.emit('updatePosition', playerData);
+        break;
+      default:
+        break;
+    }
+  }
 
   function initSky() {
 
@@ -335,17 +496,31 @@ var loadWorld = function(){
         cubes[cube_index].rotation.y += 0.02; //* (cube_index+1);
     }
 
+    checkKeyStates();
     // Update VR headset position and apply to camera.
-    controls.update();
-
+    controls.update(cx,cy,cz);
     // Render the scene.
     effect.render(scene, camera);
-
     //reticle_loop
     reticle.reticle_loop();
-
     // Keep looping.
     requestAnimationFrame(animate);
+  }
+
+  function onKeyDown( event ){
+
+      //event = event || window.event;
+
+      keyState[event.keyCode || event.which] = true;
+
+  }
+
+  function onKeyUp( event ){
+
+      //event = event || window.event;
+
+      keyState[event.keyCode || event.which] = false;
+
   }
 
   function onResize() {
@@ -392,6 +567,12 @@ var loadWorld = function(){
 
 };
 
+
+
+
+//--------------------------------------------------------------//
+//---------------------- GLOBAL FUNCTIONS ----------------------//
+//--------------------------------------------------------------//
 
 var createPlayer = function(data){
 
@@ -452,6 +633,11 @@ var updatePlayerData = function(){
     playerData.r_x = player.rotation.x;
     playerData.r_y = player.rotation.y;
     playerData.r_z = player.rotation.z;
+
+    // control camera update.
+    cx = player.position.x;
+    cy = player.position.y + 3;
+    cz = player.position.z;
 
 };
 
